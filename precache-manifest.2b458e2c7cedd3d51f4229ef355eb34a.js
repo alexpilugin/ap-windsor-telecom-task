5fc6fe8cde37043ce79a4e77055ed7a59397ebe5
Alex Pilugin <pilugin@gmail.com>
2020-10-26 05:27:58 +0000

    gh-pages

---
+++ precache-manifest.2b458e2c7cedd3d51f4229ef355eb34a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9045c9cede4bc735ba3d",
+    "url": "/ap-windsor-telecom-task/css/app.99c68647.css"
+  },
+  {
+    "revision": "d4b28695e9ab5f94f042",
+    "url": "/ap-windsor-telecom-task/css/chunk-vendors.68f8385b.css"
+  },
+  {
+    "revision": "a1329e658a457c722b2d6413d0d4351e",
+    "url": "/ap-windsor-telecom-task/index.html"
+  },
+  {
+    "revision": "5db9e5c866bf727fbb8f",
+    "url": "/ap-windsor-telecom-task/js/about.2fd49c08.js"
+  },
+  {
+    "revision": "9045c9cede4bc735ba3d",
+    "url": "/ap-windsor-telecom-task/js/app.19030bd9.js"
+  },
+  {
+    "revision": "d4b28695e9ab5f94f042",
+    "url": "/ap-windsor-telecom-task/js/chunk-vendors.cc05e4f1.js"
+  },
+  {
+    "revision": "e38929b5b2a1617716fac297936bc52e",
+    "url": "/ap-windsor-telecom-task/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ap-windsor-telecom-task/robots.txt"
+  }
+]);
